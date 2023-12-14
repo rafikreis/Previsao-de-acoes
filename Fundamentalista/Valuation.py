4cb376f8624b14f2b5cf8ba88d5f8cedd4b58ffc
rafikreis <126576142+rafikreis@users.noreply.github.com>
2023-12-14 11:17:08 -0300

    Carteira de dividendos v1
    Estou desenvolvendo uma forma de avaliar ações com base em pagamento de dividendos.

--- Fundamentalista/Valuation.py
+++ Fundamentalista/Valuation.py
@@ -1,3 +1,22 @@
+import fundamentus as fd
 import pandas as pd
-import numpy as np
-import datetime as dt
+
+ticker = fd.get_resultado()
+
+x = int(input("Escolha qual tipo de ação você deseja: \n 1-Dividendos \n 2-Alto lucro \n 3-Baratas \n:"))
+
+if x == 1:
+    a1 = ticker.drop(['roic', 'pvp', 'pcg', 'mrgliq', 'pebit', 'pacl',
+                     'evebit', 'evebitda', 'mrgebit', 'liq2m', 'psr', 'pa'], axis=1)
+    filtro = (a1['dy'] > 0.05) & (a1['divbpatr'] < 2) & (
+        a1['c5y'] > 0.05) & (a1['liqc'] > 1) & (a1['roe'] > 0.10)
+    a2 = a1[filtro]
+
+    maxmin = (a2 - a2.min()) / (a2.max() - a2.min())
+    maxmin['Nota'] = maxmin.mean(axis=1) * 100
+
+    a3 = pd.concat([a2, maxmin['Nota']], axis=1)
+
+    a4 = a3.sort_values('Nota', ascending=False)
+
+    print(a4)
